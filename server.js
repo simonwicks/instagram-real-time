079a1c9f7b942c06021affc70492574aebb38b6f
Simon Wicks <simonwicks@users.noreply.github.com>
2015-03-18 16:56:41 +0000

    Update server.js

--- server.js
+++ server.js
@@ -18,16 +18,16 @@ var pub = __dirname + '/public',
  * Set the 'client ID' and the 'client secret' to use on Instagram
  * @type {String}
  */
-var clientID = 'YOUR_CLIENT_ID',
-    clientSecret = 'YOUR_CLIENT_SECRET';
+var clientID = 'f888edeb4c364cd3988975b0833250af',
+    clientSecret = 'e3be683b20cb4d5f8e323f2f7e3b1a13';
 
 /**
  * Set the configuration
  */
 Instagram.set('client_id', clientID);
 Instagram.set('client_secret', clientSecret);
-Instagram.set('callback_url', 'http://YOUR_URL.com/callback');
-Instagram.set('redirect_uri', 'http://YOUR_URL.com');
+Instagram.set('callback_url', 'http://londonography.com/callback');
+Instagram.set('redirect_uri', 'http:londonography.com');
 Instagram.set('maxSockets', 10);
 
 /**
@@ -37,44 +37,19 @@ Instagram.set('maxSockets', 10);
  */
 Instagram.subscriptions.subscribe({
   object: 'tag',
-  object_id: 'lollapalooza',
+  object_id: 'london',
   aspect: 'media',
-  callback_url: 'http://YOUR_URL.com/callback',
+  callback_url: 'londonography.com/callback',
   type: 'subscription',
   id: '#'
 });
+
 
 /**
- * Uses the library "instagram-node-lib" to Subscribe to the Instagram API Real Time
- * with the tag "hashtag" lollapalooza2013
- * @type {String}
- */
-Instagram.subscriptions.subscribe({
-  object: 'tag',
-  object_id: 'lollapalooza2013',
-  aspect: 'media',
-  callback_url: 'http://YOUR_URL.com/callback',
-  type: 'subscription',
-  id: '#'
-});
-
-/**
- * Uses the library "instagram-node-lib" to Subscribe to the Instagram API Real Time
- * with the tag "hashtag" lolla2013
- * @type {String}
- */
-Instagram.subscriptions.subscribe({
-  object: 'tag',
-  object_id: 'lolla2013',
-  aspect: 'media',
-  callback_url: 'http://YOUR_URL.com/callback',
-  type: 'subscription',
-  id: '#'
-});
-
 // if you want to unsubscribe to any hashtag you subscribe
 // just need to pass the ID Instagram send as response to you
 Instagram.subscriptions.unsubscribe({ id: '3668016' });
+ */
 
 // https://devcenter.heroku.com/articles/using-socket-io-with-node-js-on-heroku
 io.configure(function () {
@@ -109,6 +84,7 @@ app.get("/views", function(req, res){
 
 // check subscriptions
 // https://api.instagram.com/v1/subscriptions?client_secret=YOUR_CLIENT_ID&client_id=YOUR_CLIENT_SECRET
+// https://api.instagram.com/v1/subscriptions?client_secret=f888edeb4c364cd3988975b0833250af&client_id=e3be683b20cb4d5f8e323f2f7e3b1a13
 
 /**
  * On socket.io connection we get the most recent posts
@@ -116,7 +92,7 @@ app.get("/views", function(req, res){
  */
 io.sockets.on('connection', function (socket) {
   Instagram.tags.recent({
-      name: 'lollapalooza',
+      name: 'londonography',
       complete: function(data) {
         socket.emit('firstShow', { firstShow: data });
       }
